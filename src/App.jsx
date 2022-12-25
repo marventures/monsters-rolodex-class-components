5479f471d92fa6a3239af3ca9adca86a637ed294
Marvin M. Pacis <marvedcodes@gmail.com>
2022-12-25 20:44:43 +0800

    Add keys for mapping

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,19 @@ class App extends Component {
       monsters: [
         {
           name: 'Linda',
+          id: 1,
         },
         {
           name: 'Frank',
+          id: 2,
         },
         {
           name: 'Jacky',
+          id: 3,
         },
         {
           name: 'Marvin',
+          id: 4,
         },
       ],
     };
@@ -27,7 +31,11 @@ class App extends Component {
     return (
       <div className='App'>
         {this.state.monsters.map(monster => {
-          return <h1>{monster.name}</h1>;
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
