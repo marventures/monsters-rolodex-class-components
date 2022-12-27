afa7f41d58016d79ce97e5c40d33b22393a62308
Marvin M. Pacis <marvedcodes@gmail.com>
2022-12-27 21:13:24 +0800

    Add a title and custom font

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
+import Card from '../card/Card';
 import './card-list.styles.css';
 export class CardList extends Component {
   render() {
-    console.log('render from CardList');
     const { filteredMonsters } = this.props;
 
     return (
       <div className='card-list'>
         {filteredMonsters.map(monster => {
-          const { id, name, email } = monster;
-          return (
-            <div className='card-container' key={id}>
-              <img
-                alt={`Monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} key={monster.id} />;
         })}
       </div>
     );
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './card.styles.css';
+
+export class Card extends Component {
+  render() {
+    const { id, name, email } = this.props.monster;
+    return (
+      <div className='card-container'>
+        <img
+          alt={`Monster ${name}`}
+          src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
